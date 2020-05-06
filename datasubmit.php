df0607fd59a5215585e3631b1fae7f54856539b7
yadavanuj1996 <yadavanuj@hotmail.com>
2020-05-06 15:05:36 +0530

    Restructure for heroku.

---
+++ datasubmit.php
@@ -0,0 +1,60 @@
+<?php
+	$requestBody = file_get_contents('php://input');
+	$requestObject=json_decode($requestBody);
+	if($requestObject->operation=="insert"){
+		insertGameDetails($requestBody);
+	}
+	else if($requestObject->operation=="update"){
+		$inputGid=$requestObject->gid;
+		$inputName=$requestObject->name;
+		updateName($inputGid,$inputName);
+	}
+	
+	//updateName($inputGid,$inputName);
+	//insertGameDetails("test");
+	function insertGameDetails($gameDetail){
+		//Load the file
+		$scoreDetails = file_get_contents('./data/scores.json');
+		//Decode the JSON data into a PHP array.
+		$scoreArray = json_decode($scoreDetails, true);
+		//insert gameDetail in array that has existing data
+			
+		$gameDetailArray=json_decode($gameDetail, true);
+		$i=0;
+		foreach($gameDetailArray as $key=>$value) {
+		    if($key=="operation"){
+			  unset($gameDetailArray[$key]);
+		    }
+		    $i++;
+		}
+		$updatedGameDetail=json_encode($gameDetailArray);
+		echo $updatedGameDetail;
+		array_push($scoreArray, $updatedGameDetail);
+		
+		//Encode the array back into a JSON string.
+		$json = json_encode($scoreArray);
+		//Save the file.
+		file_put_contents('./data/scores.json', $json);	
+	}
+	
+	function updateName($inputGid,$inputName){
+		//Load the file
+		$scoreDetails = file_get_contents('./data/scores.json');
+		//Decode the JSON data into a PHP array.
+		$scoreArray = json_decode($scoreDetails, true);
+		//Modify the Name property value on basis of gid key
+		foreach($scoreArray as  $key => $value){
+			$valueObject=json_decode($value);
+			if($valueObject->gid==$inputGid){
+				$valueObject->name=$inputName;
+				$scoreArray[$key]=json_encode($valueObject);
+			}
+			
+		}
+		//Encode the array back into a JSON string.
+		$json = json_encode($scoreArray);
+		//Save the file.
+		file_put_contents('./data/scores.json', $json);	
+	}
+	
+?>
---
+++ home.js
@@ -0,0 +1,54 @@
+// for more details about simplebar https://github.com/Grsmto/simplebar
+
+var difficultyLevel="Easy";
+//var myElement = document.getElementById('main-div');
+	//new SimpleBar(myElement, { autoHide: true });
+	
+document.addEventListener("DOMContentLoaded",()=>{
+	
+	document.getElementById("game-start-btn").addEventListener("click",()=>{
+		localStorage.setItem('difficulty', difficultyLevel);
+	});
+	
+	document.getElementById("cbtn1").addEventListener("click",()=>{
+		removeColorFromButton();
+		let easyButton=document.getElementById("cbtn1");
+		easyButton.classList.add("purple-background");
+		setDifficultyLevel("Easy");
+	});
+	document.getElementById("cbtn2").addEventListener("click",()=>{
+		removeColorFromButton();
+		let intermediateButton=document.getElementById("cbtn2");
+		intermediateButton.classList.add("purple-background");
+		setDifficultyLevel("Intermediate");
+	});
+	document.getElementById("cbtn3").addEventListener("click",()=>{
+		removeColorFromButton();
+		let hardButton=document.getElementById("cbtn3");
+		hardButton.classList.add("purple-background");
+		setDifficultyLevel("Hard");
+	});
+	document.getElementById("popup-btn").addEventListener("click",()=>{
+		let userName=document.getElementById("name-input").value;
+		localStorage.setItem("username",userName);
+		window.location.href="./index.html";
+	});
+	
+});
+let removeColorFromButton=()=>{
+	let easyButton=document.getElementById("cbtn1");
+	let intermediateButton=document.getElementById("cbtn2");
+	let hardButton=document.getElementById("cbtn3");
+	easyButton.classList.remove("purple-background");
+	intermediateButton.classList.remove("purple-background");
+	hardButton.classList.remove("purple-background");
+};
+
+let setDifficultyLevel=(difficulty)=>{
+	difficultyLevel=difficulty;
+}
+let addColorInButton=(buttonElement)=>{
+		buttonElement.classList.add("purple-background");
+};
+
+
